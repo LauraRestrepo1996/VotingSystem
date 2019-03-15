8fb4b29f66528d1daed7eee4a6b8f0c13a9e9225
LAURA MARITZA RESTREPO MU�OZ <laurarestrepo220579@correo.itm.edu.co>
2019-03-15 15:53:58 -0500

    Create Database with crud

---
+++ VotingSystem.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace VotingSystem.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Entities;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+
+}
---
+++ VotingSystem.Web/Data/Entities/Event.cs
@@ -0,0 +1,25 @@
+﻿
+namespace VotingSystem.Web.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        //TODO: Add Image
+    }
+
+}
